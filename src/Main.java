5469191fbd5add0536c5fcf42e814710901ca576
ChristmasLime <christmasboom@mail.ru>
2023-02-08 22:43:28 +0300

    Выполнил задачу номер 4 по другому

--- src/Main.java
+++ src/Main.java
@@ -21,17 +21,19 @@ public class Main {
     public static void task2() {
         System.out.println("Задача 2");
         int clientOS = 0;
-        int clientDeviceYear = 2015;
+        int clientDeviceYear = 2014;
         if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке.");
         } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
         } else {
-            System.out.println("Неопознаная ОС");
+            if (clientOS == 0 && clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else {
+                System.out.println("Неопознаная ОС");
+            }
         }
     }
     public static void task3() {
